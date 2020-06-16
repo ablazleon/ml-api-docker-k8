2236e232aa900c1b0dca455efe231e4723cc9df0
ablazleon <ablazleon@gmail.com>
2020-06-16 19:20:14 +0000

    feat6: k8 download link

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Download k8
+# curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+#  && chmod +x minikube
+# sudo mkdir -p /usr/local/bin/
+# sudo install minikube /usr/local/bin/
+
 # This tags and uploads an image to Docker Hub
 
 # Step 1:
